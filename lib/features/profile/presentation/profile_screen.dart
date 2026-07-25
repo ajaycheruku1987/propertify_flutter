e9d4ea9d1f9100fe3e72881e42f99c84b7f8396e
ajaycheruku <ajaycheruku1987@gmail.com>
2026-07-25 22:36:21 +0530

    build

--- lib/features/profile/presentation/profile_screen.dart
+++ lib/features/profile/presentation/profile_screen.dart
@@ -658,7 +658,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                               ),
                               const SizedBox(height: 4),
                               Text(
-                                'v7.0.35',
+                                'v7.0.36',
                                 style: TextStyle(
                                   fontSize: 10,
                                   color: Colors.grey.shade400,
