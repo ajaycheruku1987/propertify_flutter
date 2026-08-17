e9c9b099e72989d32b206b04f0e2775e12669dfa
ajaycheruku <ajaycheruku1987@gmail.com>
2026-08-17 13:18:15 +0530

    fix

--- lib/features/home/presentation/widgets/navigation_tabs.dart
+++ lib/features/home/presentation/widgets/navigation_tabs.dart
@@ -1,7 +1,6 @@
 import 'package:propertify/utils/string_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import '../../../../core/app_theme.dart';
 
 class NavigationTabs extends StatelessWidget {
   final List<String> tabs;
@@ -58,7 +57,7 @@ class NavigationTabs extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       _getTabIcon(context, index, selectedIndex == index),
-                      const SizedBox(width: 6),
+                      const SizedBox(height: 6),
                       FittedBox(
                         fit: BoxFit.scaleDown,
                         child: Text(
@@ -83,33 +82,41 @@ class NavigationTabs extends StatelessWidget {
   }
 
   Widget _getTabIcon(BuildContext context, int index, bool isSelected) {
-    String iconData;
-    switch (index) {
-      case 0:
-        iconData = 'assets/icons/document.svg';
-        break;
-      case 1:
-        iconData = 'assets/icons/agent.svg';
-        break;
-      case 2:
-        iconData = 'assets/icons/document.svg';
-        break;
-      case 3:
-        iconData = 'assets/icons/sales.svg';
-        break;
-      default:
-        iconData = 'assets/icons/sales.svg';
-    }
+    final color = isSelected
+        ? Theme.of(context).primaryColor
+        : Theme.of(context).hintColor;
 
-    return SvgPicture.asset(
-      iconData,
-      width: 24,
-      colorFilter: ColorFilter.mode(
-        isSelected
-            ? Theme.of(context).primaryColor
-            : Theme.of(context).hintColor,
-        BlendMode.srcIn,
-      ),
-    );
+    switch (index) {
+      case 0: // Feeds
+        return SvgPicture.asset(
+          'assets/icons/post.svg',
+          width: 24,
+          colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+        );
+      case 1: // Requests
+        return SvgPicture.asset(
+          'assets/icons/request.svg',
+          width: 24,
+          colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+        );
+      case 2: // Projects
+        return Icon(
+          Icons.apartment_outlined,
+          size: 24,
+          color: color,
+        );
+      case 3: // Services
+        return SvgPicture.asset(
+          'assets/icons/agent.svg',
+          width: 24,
+          colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+        );
+      default:
+        return Icon(
+          Icons.more_horiz,
+          size: 24,
+          color: color,
+        );
+    }
   }
 }
